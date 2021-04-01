ed8fb54f19a62c8d6812d50adb2b0097c0344450
Ervizal Buana <ervizal14@gmail.com>
2021-04-01 09:37:46 +0700

    Pengerjaan Tugas 5

--- I0320035_Soal 2_ Tugas 5.py
+++ I0320035_Soal 2_ Tugas 5.py
@@ -8,19 +8,19 @@ nama = str(input("Masukkan nama anda : "))
 nilai = float(input("Masukkan nilai anda : "))
 
 #IF-ELSE STATEMENT
-if(nilai < 60):
-	print("Halo, "+ nama +"! Nilai anda setelah dikonversi adalah E")
+if(nilai < 60 and nilai >= 0):
+	print("Halo, %s! Nilai anda setelah dikonversi adalah E" % nama)
 elif(nilai <= 64 and nilai >= 60):
-	print("Halo, "+ nama +"! Nilai anda setelah dikonversi adalah C")
+	print("Halo, %s! Nilai anda setelah dikonversi adalah C" % nama)
 elif(nilai <= 69 and nilai >= 65):
-	print("Halo, "+ nama +"! Nilai anda setelah dikonversi adalah C+")
+	print("Halo, %s! Nilai anda setelah dikonversi adalah C+" % nama)
 elif(nilai <= 74 and nilai >= 70):
-	print("Halo, "+ nama +"! Nilai anda setelah dikonversi adalah B")
+	print("Halo, %s! Nilai anda setelah dikonversi adalah B" % nama)
 elif(nilai <= 79 and nilai >= 75):
-	print("Halo, "+ nama +"! Nilai anda setelah dikonversi adalah B+")
+	print("Halo, %s! Nilai anda setelah dikonversi adalah B+" % nama)
 elif(nilai <= 84 and nilai >= 80):
-	print("Halo, "+ nama +"! Nilai anda setelah dikonversi adalah A-")
+	print("Halo, %s! Nilai anda setelah dikonversi adalah A-" % nama)
 elif(nilai <= 100 and nilai >= 85):
-	print("Halo, "+ nama +"! Nilai anda setelah dikonversi adalah A")
+	print("Halo, %s! Nilai anda setelah dikonversi adalah A" % nama)
 else:
 	print("Nilai yang anda masukkan tidak valid")
